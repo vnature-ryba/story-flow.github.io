2e50f99eaee67c20d5ce2ac68a6f3646ec0d8bb8
vnature-ryba <85750925+vnature-ryba@users.noreply.github.com>
2026-09-10 21:50:34 +0300

    test

--- src/Components/Common/FieldError.jsx
+++
@@ -1,8 +0,0 @@
-export default function FieldError({ show, children = 'Обязательное поле' }) {
-  if (!show) return null
-  return (
-    <p className="field-note" style={{ color: 'var(--red)', fontSize: '13px', marginTop: '5px' }}>
-      {children}
-    </p>
-  )
-}
--- src/Components/SlateStrip/SlateStrip.js
+++
@@ -1,9 +0,0 @@
-import "./Style.scss";
-
-export default function SlateStrip({color}) {
-    const style = {}
-    if (color) style['--stripe-color'] = color
-    return (
-        <div className="slate-strip" style={style}></div>
-    );
-}
